68d6df00dc7f7cda5af26f9fc45c1e9dd6d3d50d
Tõnn <tonnto@ttu.ee>
2021-03-20 19:15:09 +0200

    test mobile size

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ const darkGreen = "#8cd38b";
 const DarkerGreen = "#34b233";
 const lightGreen = "#ade0ad";
 const lightGrey = "#f7fafa";
+
+$("ul").on("click", "li", function() {
+    $(this).toggleClass("completed");
+});
