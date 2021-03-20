f8b8cd8c36c7e0eca63af739a3fe08c9b424a8f8
Tõnn <tonnto@ttu.ee>
2021-03-20 18:02:49 +0200

    Added more styling

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
+import "./index.html";
+
 const darkGreen = "#8cd38b";
 const DarkerGreen = "#34b233";
 const lightGreen = "#ade0ad";
 const lightGrey = "#f7fafa";
-
-let counter = 0;
-function addElement() {
-    var html = '<input type="range"  name="width' +     counter + '" min="40" max="200" value="40" oninput="this.form.widthPlus' + counter + '.value=this.value" />' +
-        '<input type="number" name="widthPlus' + counter + '" min="40" max="200" value="40" oninput="this.form.width'     + counter + '.value=this.value" />';
-    $(".outerContainer").append(html);
-    counter++;
-}
-
-let volume = document.getElementById("volume");
-console.log(volume);
